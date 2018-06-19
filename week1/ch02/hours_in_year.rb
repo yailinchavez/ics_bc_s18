673ce69a2540612ca283ea22f929ce07c35a1073
yailinchavez <yailinchavez27@gmail.com>
2018-06-18 19:05:17 -0700

    complete

---
+++ week1/ch02/hours_in_year.rb
@@ -0,0 +1,4 @@
+puts "regular year"
+puts 24*365
+puts "leap year"
+puts 24*366
---
+++ week1/ch02/minutes_in_decade.rb
@@ -0,0 +1,2 @@
+puts "Two leap years in a decade", 60*24*((365*8)+(366*2))
+puts "Three leap years in a decade",60*24*((365*7)+(366*3))
