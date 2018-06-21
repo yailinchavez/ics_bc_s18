b94f2bff5c90a6c7f7ce678c5f4f4973cd83ade0
yailinchavez <yailinchavez27@gmail.com>
2018-06-21 00:16:11 -0700

    complete

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,5 @@
+puts 'What is your favorite number?'
+num = gets.chomp
+num2 = 1
+num3 = num.to_i + num2
+puts 'I agree that ' + num + ' is a good number, but wouldn\'t you agree that ' + num3.to_s + ' is a bigger, and therefore better favorite number?'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Good morning. What is your name?'
+name = gets.chomp
+puts 'Do you have a middle name?'
+midname = gets.chomp
+puts 'That is a beautiful name! What is your last name, honey?'
+lastname = gets.chomp
+puts "It is very nice to meet you, " +  name + ' ' + midname + ' ' + lastname + '.'
