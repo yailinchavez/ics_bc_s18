74873435925e2abe69ddac5b4b1118d1c803545d
yailinchavez <yailinchavez27@gmail.com>
2018-06-22 17:27:33 -0700

    complete

---
+++ week2/ch06/table_of_contents.rb
@@ -0,0 +1,6 @@
+line_wid = 50
+puts ('Table of Contents'.center(line_wid))
+ch_line_wid = 20
+puts ('Chapter 1: The Prison-Door ') + ('page 1'.rjust(ch_line_wid))
+puts ('Chapter 2: The Market-Place') + ('page 3'.rjust(ch_line_wid))
+puts ('Chapter 3: The Recognition ') + ('page 12'.rjust(ch_line_wid))
