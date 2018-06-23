1ebe2c98be5890997a609d4fbe9830228ade1c5f
yailinchavez <yailinchavez27@gmail.com>
2018-06-22 17:07:21 -0700

    complete

---
+++ week2/ch06/angry_boss.rb
@@ -0,0 +1,4 @@
+puts 'WHAT IN THE WORLD DO YOU WANT NOW?!'
+answer = gets.chomp
+puts 'WHAT DO YOU MEAN ' + '"' + answer.upcase + '"' + '?!' + ' WHAT KIND OF AUTHORITY DO YOU THINK YOU HAVE?!'
+puts 'YOU\'RE FIRED!!!'
